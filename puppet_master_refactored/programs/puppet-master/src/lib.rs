630866b108afcf292d3bb79c232a12b44f3ae2f0
Sebastian Dine <sdine.info@gmail.com>
2022-08-28 17:52:41 +0000

    puppet master refactored

---
+++ puppet_master_refactored/programs/puppet-master/src/lib.rs
@@ -0,0 +1,59 @@
+use anchor_lang::prelude::*;
+use std::mem::size_of;
+
+use puppet::cpi::accounts::SetData;
+use puppet::program::Puppet;
+use puppet::{self, Data};
+
+declare_id!("HmbTLCmaGvZhKnn1Zfa1JVnp7vkMV4DYVxPLWBVoN65L");
+
+#[program]
+mod puppet_master {
+    use super::*;
+
+    pub fn initialize(ctx: Context<CreateMasterPdaAccount>) -> Result<()> {
+        ctx.accounts.master_pda_account.authority = ctx.accounts.authority.key();
+        Ok(())
+    }
+
+    pub fn pull_strings(ctx: Context<PullStrings>, bump: u8, data: u64) -> Result<()> {
+        let cpi_program = ctx.accounts.puppet_program.to_account_info();
+        // create instruction builder struct, this is similar to the Accounts struct
+        let cpi_accounts = SetData {
+            puppet_account: ctx.accounts.puppet_account.to_account_info(),
+            authority: ctx.accounts.master_pda_account.to_account_info()
+        };
+        // create cpi context
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        // call function with cpi context and additional parameter
+        let master_pda_auth_key = ctx.accounts.authority.key();
+        let master_pda_bump = &[bump];
+        let seeds = &[&[b"master", master_pda_auth_key.as_ref(), master_pda_bump][..]];
+        puppet::cpi::set_data(cpi_ctx.with_signer(seeds), data)        
+    }
+}
+
+#[derive(Accounts)]
+pub struct CreateMasterPdaAccount<'info> {
+    #[account(init, payer = authority, seeds = [b"master", authority.key().as_ref()], bump, space = 8 + size_of::<MasterPdaAccount>())]
+    pub master_pda_account: Account<'info, MasterPdaAccount>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct PullStrings<'info> {
+    #[account(mut)]
+    pub puppet_account: Account<'info, Data>,
+    pub puppet_program: Program<'info, Puppet>,
+    #[account(has_one = authority)]
+    pub master_pda_account: Account<'info, MasterPdaAccount>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct MasterPdaAccount {
+    pub authority: Pubkey
+}
---
+++ puppet_master_refactored/tests/puppet.ts
@@ -0,0 +1,80 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { expect } from 'chai';
+import { Puppet } from '../target/types/puppet';
+import { PuppetMaster } from '../target/types/puppet_master';
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+
+const findPDAforPuppet = async (programId: anchor.web3.PublicKey, authority: anchor.web3.PublicKey): Promise<anchor.web3.PublicKey> => {
+
+  const [pda, _bump] = await anchor.web3.PublicKey.findProgramAddress(
+    [utf8.encode('puppet'), authority.toBytes()],
+    programId
+  );
+  return pda;
+}
+
+const findPDAforMaster = async (programId: anchor.web3.PublicKey, authority: anchor.web3.PublicKey) => {
+
+  const [pda, bump] = await anchor.web3.PublicKey.findProgramAddress(
+    [utf8.encode('master'), authority.toBytes()],
+    programId
+  );
+  return {pda: pda, bump: bump};
+}
+
+describe('puppet', () => {
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const puppetProgram = anchor.workspace.Puppet as Program<Puppet>;
+  const puppetMasterProgram = anchor.workspace.PuppetMaster as Program<PuppetMaster>;
+  
+  before(async () => {
+
+    const masterPDA = await findPDAforMaster(puppetMasterProgram.programId, provider.wallet.publicKey);
+    const puppetPDA = await findPDAforPuppet(puppetProgram.programId, masterPDA.pda);
+
+    // create "puppet master pda account" with authority "provider.wallet"
+    await puppetMasterProgram.methods
+      .initialize()
+      .accounts({
+          masterPdaAccount: masterPDA.pda,
+          authority: provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId
+      })
+      .rpc();
+
+    // create "puppet pda account" with authority "puppet master pda account"
+    await puppetProgram.methods
+      .initialize()
+      .accounts({
+        puppetAccount: puppetPDA,
+        authorityPda: masterPDA.pda,
+        payer: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+
+      })
+      .rpc();
+  });
+
+  it('PDA-signed CPI is working.', async () => {
+
+    const masterPDA = await findPDAforMaster(puppetMasterProgram.programId, provider.wallet.publicKey);
+    const puppetPDA = await findPDAforPuppet(puppetProgram.programId, masterPDA.pda);
+
+    await puppetMasterProgram.methods
+        .pullStrings(masterPDA.bump, new anchor.BN(42))
+        .accounts({
+            puppetProgram: puppetProgram.programId,
+            puppetAccount: puppetPDA,
+            masterPdaAccount: masterPDA.pda,
+            authority: provider.wallet.publicKey,
+
+        })
+        .rpc();
+
+    expect((await puppetProgram.account.data
+      .fetch(puppetPDA)).data.toNumber()).to.equal(42);
+  });
+});
