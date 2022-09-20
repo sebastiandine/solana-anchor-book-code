e907d358808db9405e437ba3a444902841e0f346
Sebastian Dine <sebastiandine91@gmail.com>
2022-09-20 16:58:30 +0200

    Nft tools (#6)
    * create collection
    
    * printing
    
    Co-authored-by: Sebastian Dine <sdine.info@gmail.com>

---
+++ metaplex_scripts/scripts/print_edition.ts
@@ -0,0 +1,30 @@
+import * as anchor from "@project-serum/anchor";
+import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
+
+const printEdition = async () => {
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const metaplex = new Metaplex(provider.connection).use(walletAdapterIdentity(provider.wallet));
+
+
+  const nft = await metaplex.nfts().create({
+    name: "Happy Star",
+    symbol: "STR",
+    uri: "https://raw.githubusercontent.com/sebastiandine/temp-testing/main/star.json",
+    sellerFeeBasisPoints: 500,
+    maxSupply: new anchor.BN(100)
+
+  }).run();
+
+  metaplex.nfts().printNewEdition({
+    originalMint: nft.mintAddress
+  }).run();
+
+}
+
+printEdition().then(
+  (result) => {console.log("success")},
+  (reason) => {console.log("failed", reason)}
+);
